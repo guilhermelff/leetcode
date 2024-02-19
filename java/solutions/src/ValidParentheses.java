d3e8b6f3233336f655a420d5935e55c8aa033b33
Guilherme Fernandes <guilhermelff@id.uff.br>
2024-02-19 16:00:38 -0300

    Create ValidParentheses.java

---
+++ java/solutions/src/ValidParentheses.java
@@ -0,0 +1,18 @@
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+        // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+        return stack.isEmpty();
+    }
+}
